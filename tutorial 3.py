35264fc39a5777feaa414b16d5dcbfe03b621db2
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2022-09-10 14:07:03 +0530

    video display

--- tutorial 3.py
+++ tutorial 3.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 def home():
@@ -9,6 +9,19 @@ def home():
 @app.route("/test")
 def test():
     return render_template("new.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    #return render_template("login.html")
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
